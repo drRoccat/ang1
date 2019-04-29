b65f2978f173643410d2ad985e37575cf090774f
Vlad <belka422@gmail.com>
2019-04-29 04:23:27 +0200

    1 day

---
+++ src/app/cars/cars.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html',
+  styleUrls: ['./cars.component.css']
+})
+export class CarsComponent  {
+
+  carName = '';
+  addCarStatus = false;
+  cars = ['Ford', 'Audi', 'BMW'];
+  items = [{id: 3, name: 'item 1'}, {id: 6, name: 'item 2'}, {id: 9, name: 'item 3'}];
+
+  constructor() {
+
+  }
+
+
+  addCar() {
+    this.addCarStatus = true;
+    this.cars.push(this.carName);
+    this.carName = '';
+  }
+
+
+}
